d72055c21c2078cf6a8f2ad07fa648acb2048f40
SasukeSyntax <koolcoding106@gmail.com>
2026-04-21 15:22:12 +0530

    adding task function

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 const task = document.getElementById("input");
-
+const list = document.getElementById("list");
 
 function addtask(){
-    
+    const newTag = document.createElement("li");
+
+    const content = task.value;
+
+    newTag.textContent = content;
+
+    list.appendChild(newTag);
+
 }
+
